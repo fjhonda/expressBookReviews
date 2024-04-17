4f5740f1c537b36280f3f41d253356911a48ea95
Francisco Lemus <fjmorataya1@gmail.com>
2024-04-16 21:29:16 -0400

    Added endpoint for publish a book review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -25,13 +25,39 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  
+    const {username, password} = req.body
+
+    if (!username || !password)
+        return res.send("You must provide a username and a password to login")
+
+    if (authenticatedUser(username, password)){
+        const accessToken  = jwt.sign({
+            data: password,
+        }, "access", { expiresIn: 60*60})
+
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.send("User logged successfully")
+    }
+
+    return res.send("Your credentials couldn't login")
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const {username} = req.session.authorization
+  const {isbn} = req.params.isbn
+  const {review} = req.body
+
+  const currentBook = books[isbn]
+
+  if (!currentBook)
+        return res.send(404).json({message: "Book not found"})
+
+    currentBook.reviews[username] = review
+    res.send("Your review was published successfully")
+
 });
 
 module.exports.authenticated = regd_users;
